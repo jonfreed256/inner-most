2faabbdff2127c5928f70c99e4ccec4470838b6e
Elizabeth Le <elizabethlemk@yahoo.com>
2019-04-13 14:03:32 -0400

    gradient

--- inner-most-front/src/components/NavBar.js
+++ inner-most-front/src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button, Container, Header, Icon, Form } from "semantic-ui-react";
+import { Gradient } from "react-gradient";
 
 class NavBar extends React.Component {
   state = {
@@ -25,27 +26,39 @@ class NavBar extends React.Component {
       )
       .header("Content-Type", "application/x-www-form-urlencoded")
       .send(`text=I don't like to eat spinach or kale.`)
-      .end(result => console.log(result.status, result.headers, result.body));
+      .end(result => console.log(result.body));
   };
 
   render() {
     console.log(this.state);
+    const gradients = [["#bd19d6", "#ea7d10"], ["#ff2121", "#25c668"]];
     return (
-      <Container textAlign="center" id="twitter-form">
-        <Header as="h2" icon textAlign="center">
-          <Header.Content>Inner Most</Header.Content>
-          <Icon name="user" circular />
-        </Header>
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Input
-            value={this.state.username}
-            onChange={this.handleChange}
-            placeholder="type your twitter username"
-            style={{ maxWidth: "200px" }}
-          />
-          <Button> Submit </Button>
-        </Form>
-      </Container>
+      <Gradient
+        gradients={gradients} // required
+        property="background"
+        duration={3000}
+        angle="45deg"
+      >
+        <Container
+          textAlign="center"
+          id="twitter-form"
+          style={{ width: "100vw", height: "100vh" }}
+        >
+          <Header as="h2" icon textAlign="center" inverted>
+            <Header.Content>Inner Most</Header.Content>
+            <Icon name="user" inverted />
+          </Header>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Input
+              value={this.state.username}
+              onChange={this.handleChange}
+              placeholder="type your twitter username"
+              style={{ maxWidth: "200px" }}
+            />
+            <Button> Submit </Button>
+          </Form>
+        </Container>
+      </Gradient>
     );
   }
 }
